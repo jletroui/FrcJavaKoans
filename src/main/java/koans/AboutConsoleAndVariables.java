83a55b3c23d4a05f185dd06e8cb72a29c7db7916
Julien Letrouit <julien.letrouit@gmail.com>
2023-05-17 20:19:55 -0400

    Better formatting in comments

--- src/main/java/koans/AboutConsoleAndVariables.java
+++ src/main/java/koans/AboutConsoleAndVariables.java
@@ -10,13 +10,13 @@ public class AboutConsoleAndVariables {
      * 
      * All lines of code in Java must end with the ';' character. Ex:
      * 
-     * System.out.println("Apple");
+     *      System.out.println("Apple");
      * 
      * You can use the method System.out.println([some value]) to write something in the console.
      * 
      * You can tell Java that some value is text by enclosing it between double quotes. Ex:
      * 
-     * "This is text"
+     *      "This is text"
      * 
      * -------------------------------
      * 
@@ -35,10 +35,10 @@ public class AboutConsoleAndVariables {
      * 
      * In Java, you can compute arithmetic expressions in Java. For example:
      * 
-     * 3 + 4
-     * 3 * 4
-     * 3 / 4
-     * 3 - 4
+     *      3 + 4
+     *      3 * 4
+     *      3 / 4
+     *      3 - 4
      * 
      * You can use the method System.out.println([some arithmetic expression]) to do so.
      * 
@@ -63,24 +63,24 @@ public class AboutConsoleAndVariables {
      * For example, when the information is an integer number, the type is called 'int'.
      * Putting everything together, if we want to create a variable for the number of legs of a pig:
      * 
-     * int pigNbOfLegs = 4;
-     *  ^       ^       ^
-     * type   name    value
+     *      int pigNbOfLegs = 4;
+     *       ^       ^       ^
+     *      type   name    value
      * 
      * The value can be an arithmetic expression. For example:
      * 
-     * int pigNbOfLegs = 1 + 1 + 1 + 1;
+     *      int pigNbOfLegs = 1 + 1 + 1 + 1;
      * 
      * Whenever you use the name of the variable, Java will replace it with its value when running.
      * For example, if you want to display the number of legs of a pig variable in the console:
      * 
-     * System.out.println(pigNbOfLegs);
-     *                         ^
-     *     'pigNbOfLegs' will be replaced by its value.
+     *      System.out.println(pigNbOfLegs);
+     *                              ^
+     *          'pigNbOfLegs' will be replaced by its value.
      * 
-     * This is equivalent to:
+     * So this is equivalent to:
      * 
-     * System.out.println(4);
+     *      System.out.println(4);
      * 
      * -------------------------------
      * 
@@ -120,11 +120,11 @@ public class AboutConsoleAndVariables {
      * 
      * Let's say that when asked, the user will type 'Hello' then the [ENTER] key in the console. Then:
      * 
-     * String someText = readLine();
+     *      String someText = readLine();
      * 
      * Will become the equivalent to:
      * 
-     * String someText = "Hello";
+     *      String someText = "Hello";
      * 
      * -------------------------------
      * 
@@ -147,28 +147,28 @@ public class AboutConsoleAndVariables {
      * You will need to create a text out of 2 text values. Glueing multiple text values together is called "concatenating".
      * In Java, you can do that with the '+' operator:
      * 
-     * String glued = "abc" + "def";
-     *          ^
-     *  glued's value is "abcdef"
+     *      String glued = "abc" + "def";
+     *               ^
+     *       glued's value is "abcdef"
      * 
      * Warning! Don't confuse the String concatenation operator with the number's 'addition' operator. This won't work:
      * 
-     * 4 + "abc"
-     *   ^
-     * Error: because the left of the operator is a number, '+' is the 'addition' operator for numbers, not the text's 'concatenate' one.
-     * As a result, Java expects an other number to the right of the '+', but "abc" is not a number.
+     *      4 + "abc"
+     *        ^
+     *      Error: because the left of the operator is a number, '+' is the 'addition' operator for numbers, not the text's 'concatenate' one.
+     *      As a result, Java expects an other number to the right of the '+', but "abc" is not a number.
      * 
      * Note: however, this will work:
      * 
-     * String glued = "abc" + 4;
-     *          ^
-     *  glued's value is "abc4"
+     *      String glued = "abc" + 4;
+     *               ^
+     *       glued's value is "abc4"
      * 
      * This is because Java can convert anything, including numbers, to text automatically. So in this case, it is equivalent to:
      * 
-     * String glued = "abc" + "4";
-     *                         ^
-     *            Automatically converted to text
+          * String glued = "abc" + "4";
+     *                              ^
+     *                 Automatically converted to text
      * You can read a text from the console with readLine(). This method is _returning_ the value typed in the console by the user.
      * 
      * -------------------------------
@@ -190,9 +190,9 @@ public class AboutConsoleAndVariables {
      * 
      * You can concatenate more than 2 texts together. For example:
      * 
-     * String glued = "abc" + "def" + "ghi";
-     *          ^
-     *  glued's value is "abcdefghi"
+     *      String glued = "abc" + "def" + "ghi";
+     *               ^
+     *       glued's value is "abcdefghi"
      * 
      * -------------------------------
      * 
@@ -218,18 +218,18 @@ public class AboutConsoleAndVariables {
      * But, in order to compute the 5-years-from-now value, you will need to convert the text to an integer number first.
      * You can convert some text to an integer like so:
      * 
-     * String nbOfOrangesAsText = "3";
-     * int nbOfOrangesAsNumber = Integer.parseInt(nbOfOrangesAsText);
-     *              ^
-     *    value is 3, as a number
+     *      String nbOfOrangesAsText = "3";
+     *      int nbOfOrangesAsNumber = Integer.parseInt(nbOfOrangesAsText);
+     *                   ^
+     *         value is 3, as a number
      * 
      * For the computation, you can either create a new variable you can display later:
      * 
-     * int nbOfOragesAfterIAteOne = nbOfOrangesAsNumber - 1;
+     *     int nbOfOragesAfterIAteOne = nbOfOrangesAsNumber - 1;
      * 
      * Or compute it directly where you need it, by using parentheses:
      * 
-     * System.out.println("Oranges left: " + (nbOfOrangesAsNumber - 1));
+     *     System.out.println("Oranges left: " + (nbOfOrangesAsNumber - 1));
      * 
      * -------------------------------
      * 

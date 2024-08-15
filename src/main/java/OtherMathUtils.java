a79217078eda0ec675183f083101269a65f66b74
Andyroboboy25 <94418039+Andyroboboy25@users.noreply.github.com>
2024-08-14 20:00:42 -0500

    Add files via upload

---
+++ src/main/java/OtherMathUtils.java
@@ -0,0 +1,21 @@
+package utils;
+
+import koans.english.AboutMoreMethods;
+
+public class OtherMathUtils {
+
+    public static int max(int num1, int num2){
+
+        if(num1 > num2){
+
+            return num1;
+
+        } else {
+
+            return num2;
+
+        }
+
+    }
+    
+}
